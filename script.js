f7934cdbc8c1e0a11cf3936da43ef006e9eb6219
UtsabMagar3 <utsabthamimagar@gmail.com>
2024-07-06 22:57:16 +0545

    javascript for hamburger icon

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu{
+	const menu = document.querySelector(".menu-links");
+	const menu = document.querySelector(".hamburger-icon");
+	menu.classlist.toggle("open");
+	icon.classlist.toggle("open")
+
+
+} 
