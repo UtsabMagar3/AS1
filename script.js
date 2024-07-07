216742c60e028c5e708029438ef8ff8fadcf0edd
UtsabMagar3 <utsabthamimagar@gmail.com>
2024-07-07 12:09:32 +0545

    deleted js file

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu{
-	const menu = document.querySelector(".menu-links");
-	const menu = document.querySelector(".hamburger-icon");
-	menu.classlist.toggle("open");
-	icon.classlist.toggle("open")
-
-
-} 
